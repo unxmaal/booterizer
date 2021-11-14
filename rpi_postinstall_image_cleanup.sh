bd7d82227292a6a5a588aad582f4fbf5d592094c
Eric Dodd <eric.e.dodd@gmail.com>
2021-11-14 09:29:04 -0500

    cleaned up media tarballs

--- rpi_postinstall_image_cleanup.sh
+++ rpi_postinstall_image_cleanup.sh
@@ -21,6 +21,30 @@ echo "Cleaning apt caches."
 apt-get clean -y
 apt-get autoclean -y
 
+echo "Cleaning up install media"
+rm /vagrant/irix/6.5.30/Overlay/disc1/disc1.tar.gz
+rm /vagrant/irix/6.5.30/Overlay/disc2/disc2.tar.gz
+rm /vagrant/irix/6.5.30/Overlay/disc3/disc3.tar.gz
+rm /vagrant/irix/6.5.30/Overlay/apps/apps.tar.gz
+rm /vagrant/irix/6.5.30/Overlay/capps/capps.tar.gz
+rm /vagrant/irix/6.5.22/Overlay/disc1/disc1.tar.gz
+rm /vagrant/irix/6.5.22/Overlay/disc2/disc2.tar.gz
+rm /vagrant/irix/6.5.22/Overlay/disc3/disc3.tar.gz
+rm /vagrant/irix/6.5.22/Overlay/apps/apps.tar.gz
+rm /vagrant/irix/Development/devlibs/developmentlibraries.tar.gz
+rm /vagrant/irix/Development/mipspro_ap/mipsproap.tar.gz
+rm /vagrant/irix/Development/mipspro_c/mipspro_c.tar.gz
+rm /vagrant/irix/Development/mipspro_cee/mipspro_cee.tar.gz
+rm /vagrant/irix/Development/mipspro_cpp/mipspro_cpp.tar.gz
+rm /vagrant/irix/Development/mipspro_update/mipspro744update.tar.gz
+rm /vagrant/irix/Development/mipspro/mipspro-7.4.3m.tar
+rm /vagrant/irix/Development/prodev/prodev.tar.gz
+rm /vagrant/irix/Extras/sgifonts/sgipostscriptfonts.tar.gz
+rm /vagrant/irix/Extras/perfcopilot/perfcopilot.tar.gz
+rm /vagrant/irix/Foundation/disc1/foundation1.tar.gz
+rm /vagrant/irix/Foundation/disc2/foundation2.tar.gz
+rm /vagrant/irix/Foundation/nfs/onc3nfs.tar.gz
+
 echo "Zeroing free space. This will take a while."
 dd if=/dev/zero of=/EMPTY bs=1M 
 rm -f /EMPTY
