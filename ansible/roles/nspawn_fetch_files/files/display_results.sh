1a28d0e22211a28806d072580b6f213d8060b467
Eric Dodd <eric.e.dodd@gmail.com>
2025-05-05 13:45:59 -0400

    fixed fetch files

--- ansible/roles/nspawn_fetch_files/files/display_results.sh
+++ ansible/roles/nspawn_fetch_files/files/display_results.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+if [[ -d /irix ]] ; then
+  IDIR=/irix
+elif [[ -d /srv/irix ]] ; then
+  IDIR=/srv/irix
+else
+  echo "IRIX media dir missing!"
+  exit 1
+fi
 
 ## Display some useful info about the IRIX files
-cd /irix
+cd "$IDIR"
 
 echo "__________________  Partitioners found __________________"
 find . -name "fx.*" -type f | sed 's#./#bootp():/#' | sort
